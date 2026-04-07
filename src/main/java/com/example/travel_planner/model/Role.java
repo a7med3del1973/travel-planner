3ca851e999b6d8ddd3842019ccb54aa6c7eef1cb
a7med3del1973 <a7med3del1973@gmail.com>
2026-04-07 13:18:34 +0200

    Update project files

--- src/main/java/com/example/travel_planner/model/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.travel_planner.model;
-
-public enum Role {
-    ADMIN,
-    USER
-}
--- src/main/java/com/example/travel_planner/service/VisitService.java
+++
@@ -1,5 +0,0 @@
-package com.example.travel_planner.service;
-
-public interface VisitService {
-    void markWantToVisit(Long userId, Long destinationId);
-}
